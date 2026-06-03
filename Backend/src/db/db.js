1d31c2ce46194a8a6b0687e0af213c9412ffe138
PravallikaP08 <pravallikaponnada8@gmail.com>
2026-06-03 10:48:51 +0530

    debug env

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 function connectDB() {
+  console.log("ENV KEYS:", Object.keys(process.env));
+  console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
   mongoose
     .connect(process.env.MONGODB_URI)
-    .then(() => {
-      console.log("MongoDB connected");
-    })
-    .catch((err) => {
-      console.log("MongoDB connection error:", err);
-    });
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB connection error:", err));
 }
+
 module.exports = connectDB;
